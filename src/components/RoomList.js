1a7b587a73ef4324cce0759b8e659b7c39c33a2c
Jason <jasongonzalez-whitsell@hotmail.com>
2018-11-27 02:07:38 -0600

    checkpoint-3-create-chat-rooms-completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,10 +4,11 @@ class RoomList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rooms: []
+            rooms: [],
+            value: ''
         };
-        console.log(this.props);
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -20,12 +21,37 @@ class RoomList extends Component {
      });
    }
 
+   handleChange(event) {
+       this.setState({
+           value: event.target.value
+       });
+   }
+
+   createRoom(newRoom){
+       if(newRoom === ''){
+           return;
+       }
+       else{
+       this.roomsRef.push({
+            name: newRoom
+       });
+       this.setState({
+           value: ''
+       })
+    }
+   }
+
    render(){
        return(
         <section>
-            {this.state.rooms.map((room,index) =>
+            {this.state.rooms.map((room) =>
                 <p key = {room.key}>{room.name}</p>
             )}
+
+            <form>
+                <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Create a new room"/><br/>
+                <input type="button" value="Add room" onClick={this.createRoom.bind(this, this.state.value)} />
+            </form>
         </section>
        );
    }
